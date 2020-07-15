6da11a2d91a084f9663be819cb6ac7e50172f2d4
Dhaval Mehta <idhavalmehta@gmail.com>
2020-07-15 15:23:59 +0530

    Fix deployment issues

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/vue-heroes-tutorial/' : '/'
+    process.env.NODE_ENV === 'production' ? '/tutorial-heroes-vue/' : '/'
 };
