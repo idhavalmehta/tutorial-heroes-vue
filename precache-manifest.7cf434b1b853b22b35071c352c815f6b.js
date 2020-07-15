15911523e789e0524dc4dddb007db43234d234ab
Dhaval Mehta <idhavalmehta@gmail.com>
2020-07-15 15:23:53 +0530

    deploy

---
+++ precache-manifest.7cf434b1b853b22b35071c352c815f6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee320211158ebdd79366",
+    "url": "/tutorial-heroes-vue/css/app.04004764.css"
+  },
+  {
+    "revision": "9cc78b0e31672f3eb30475de722f2513",
+    "url": "/tutorial-heroes-vue/index.html"
+  },
+  {
+    "revision": "ee320211158ebdd79366",
+    "url": "/tutorial-heroes-vue/js/app.1cffff7d.js"
+  },
+  {
+    "revision": "487d0fb05696b2908e86",
+    "url": "/tutorial-heroes-vue/js/chunk-vendors.671a8b97.js"
+  },
+  {
+    "revision": "fd2dddc1420837a7c11b17a510716ab9",
+    "url": "/tutorial-heroes-vue/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/tutorial-heroes-vue/robots.txt"
+  }
+]);
